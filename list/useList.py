784311a56e69af9a1a87ab0624e875f16921be92
arjun289 <arjun289singh@gmail.com>
2018-02-12 01:01:55 +0530

    using linked list

---
+++ list/useList.py
@@ -0,0 +1,18 @@
+from linked import LinkedList
+from linked import Node
+
+
+if __name__ == '__main__':
+    llist = LinkedList()
+    llist.head = Node(1)
+    second = Node(2)
+    third = Node(3)
+
+    llist.head.next = second
+    second.next = third
+    
+    llist.printList()
+
+
+
+ 
